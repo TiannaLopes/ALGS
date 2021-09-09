3d0ee37248d6f18fc8117cb3f39e9cca8466a2d7
Tianna Lopes <63357829+TiannaLopes@users.noreply.github.com>
2021-09-09 10:07:03 -0400

    Update ValidateSubSeq.js

--- ValidateSubSeq.js
+++ ValidateSubSeq.js
@@ -1,1 +1,20 @@
 // Validate sub.seq
+
+//Given two non-emoty arrays of integers, write a function that determines whether the second array is a subsequence of the first
+
+
+function validate(arr, seq){
+ let sIndex =0;
+  for(const value of arr){
+    if(sindex === seq.length) 
+      break;
+    if(seq[sIndex] === value) 
+      sIndex++;
+  }
+  return sIndex === seq.length;
+  
+}
+
+
+
+

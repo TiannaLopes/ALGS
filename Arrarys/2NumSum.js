e66b78685c4d01a6ef3d311dfa11ae269b6bc224
Tianna Lopes <63357829+TiannaLopes@users.noreply.github.com>
2021-09-10 14:23:23 -0400

    Update 2NumSum.js

--- Arrarys/2NumSum.js
+++ Arrarys/2NumSum.js
@@ -1,1 +1,19 @@
 
+const twoumsum = (array, final) => {
+    let mapOfNumbers = {};
+        let twoIndexes = [];
+
+        for (let i = 0; i < array.length; i++) {
+        mapOfNumbers[array[i]] = i;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+          let target = final - arr[i];
+      if(mapOfNumbers[target] !== null && mapOfNumbers[target] !== i) {
+        twoIndexes.push(i);
+            twoIndexes.push(mapOfNumbers[target]);
+      }
+        }
+
+      return twoIndexes;
+}

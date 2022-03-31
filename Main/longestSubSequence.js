e2ff8d59a226afadf9f2c086ee8e87c39f6ebed2
Tianna Lopes <63357829+TiannaLopes@users.noreply.github.com>
2022-03-31 14:10:04 -0400

    Update and rename 111.js to Main/longestSubSequence.js

--- Main/longestSubSequence.js
+++ Main/longestSubSequence.js
@@ -1,8 +1,3 @@
 Describe an algorithm to compute the longest increasing subsequence of an array of numbers in O(n log n) time.
 
 
-
-
-
-
-Given an array of integers, find the maximum XOR of any two elements
